1bc55cb2c944483fd6a271e91c94fad282c665fd
ticincloud <44474450+ticincloud@users.noreply.github.com>
2021-01-27 18:47:35 +0100

    Delete bootstrap directory

--- bootstrap/js/agora.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('#bLogin').on('click', function() { $('.opcioMenu').removeClass('disabled'); });
-    $('#loginModal').on('shown.bs.modal', function() {
-        $('#inputEmail').focus();
-    });
-});
